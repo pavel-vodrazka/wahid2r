e98d3fe5e09b2c6f17f61164cbd7742869d7f72a
Pavel Vodrazka <vodrazka@svujihlava.cz>
2015-02-16 11:19:54 +0100

    Package documentation added.

---
+++ R/wahid2r.R
@@ -0,0 +1,14 @@
+#' wahid2r: A package for scraping data on the occurence of OIE listed animal
+#' diseases from the OIE WAHID website.
+#'
+#' The wahid2r package provides functions to download Summaries of Immediate
+#' notifications and Follow-ups, Summaries of outbreaks, and Outbreak reports on
+#' all OIE-listed animal diseases (since 2005) from the OIE WAHID website
+#' (\url{http://www.oie.int/wahis_2/public/wahid.php/Diseaseinformation/Immsummary}).
+#'
+#' @section Warning: The data might be incomplete, depending on the quality of
+#'   reporting by the individual countries. Use only for information purposes.
+#'
+#' @docType package
+#' @name wahid2r
+NULL
