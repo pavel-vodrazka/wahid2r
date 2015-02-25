395e1a47ea427e0f27d13c2d0578748e10e73217
Pavel Vodrazka <vodrazka@svujihlava.cz>
2015-02-25 16:33:31 +0100

    match_countries() documented.

--- R/countries.R
+++ R/countries.R
@@ -127,8 +127,46 @@ get_countries <- function(print_only = FALSE,
   return(countries)
 }
 
+#' Matches patterns entered against the table of countries available on the OIE
+#' WAHID website
+#'
+#' Utility function \code{match_countries} is used by other functions from the
+#' package to match country or area specification against the
+#' \code{countries_available} table.
+#'
+#' The country or area specification can be entered as a pattern or a character
+#' vector of patterns. In default, it is matched against all fields in the
+#' \code{countries_available} table and ISO3 codes of countries matched are
+#' returned as \code{character} vector. Matching is case-insensitive. Fields to
+#' match against and fields returned can be specified. Multiple fields are
+#' returned as \code{tbl_df}.
+#'
+#' @param x A \code{character} scalar or vector containing a pattern or patterns
+#'   to search for.
+#' @param fields A \code{character} scalar or vector giving the names of fields
+#'   to match against. Possible values are "all" (default) or any combination of
+#'   column names of the \code{countries_available}.
+#' @param select A \code{character} scalar or vector giving the names of fields
+#'   to be returned. Possible values are "all" or any combination of column
+#'   names of the \code{countries_available} (default "ISO3").
+#' @param simplify A \code{logical} scalar specifying whether the result should
+#'   be simplified to character vector if only one field has to be returned.
+#'
+#' @return A \code{character} vector or \code{tbl_df} containing the selected
+#'   columns from the \code{countries_available} for records that were matched
+#'   by the pattern(s) entered. \code{NULL} if there is no match or any of the
+#'   arguments is specified incorrectly.
+#'
+#' @examples
+#' match_countries("Europe", fields = "OIE_region")
+#' match_countries("^East.*Europe", select = "all")
+#' match_countries("Caribbean", fields = "GEO3")
+#' match_countries("Congo", select = "all")
+#' match_countries("Kongo")
+#'
 #' @export
-match_countries <- function(x, fields = "all", select = "ISO3") {
+match_countries <- function(x, fields = "all", select = "ISO3",
+                            simplify = TRUE) {
   UseMethod("match_countries")
 }
 
@@ -139,12 +177,22 @@ match_countries.default <- function(x, ...) {
   return(NULL)
 }
 
+#' @describeIn match_countries
+#' @importFrom dplyr union
+#' @importFrom magrittr %>%
 #' @export
-match_countries.character <- function(x, fields = "all", select = "ISO3") {
+match_countries.character <- function(x, fields = "all", select = "ISO3",
+                                      simplify = TRUE) {
   message("- Matching countries entered.")
   if (length(x) == 0 || is.na(x)) {
     message("- Function match_countries(): incorrect specification of ",
             "countries to be matched: ", capture.output(str(x)), ".")
+    return(NULL)
+  }
+  if (!is.logical(simplify) || is.na(simplify)) {
+    message("- Function match_countries(): incorrect specification of ",
+            "the \"simplify\" argument: ", capture.output(str(simplify)), ", ",
+            "possible values are: TRUE or FALSE.")
     return(NULL)
   }
   if(!exists("countries_available", where = globals, inherits = FALSE)) {
@@ -155,25 +203,38 @@ match_countries.character <- function(x, fields = "all", select = "ISO3") {
     message("- Function match_countries(): incorrect specification of ",
             "fields to be matched against: ", capture.output(str(fields)), ", ",
             "possible values are: \"all\" or any combination of \"",
-            paste0(names(globals$countries_available), collapse = "\", \""), "\".")
+            paste0(names(globals$countries_available), collapse = "\", \""),
+            "\".")
     return(NULL)
   }
-  if (fields == "all") fields <- names(globals$countries_available)
+  f <- if (length(fields) == 1 && fields == "all") {
+    names(globals$countries_available)
+    } else fields
   if (!is.character(select)
       || select != "all" && !select %in% names(globals$countries_available)) {
     message("- Function match_countries(): incorrect specification of ",
             "fields to be selected: ", capture.output(str(select)), ", ",
             "possible values are: \"all\" or any combination of \"",
-            paste0(names(globals$countries_available), collapse = "\", \""), "\".")
+            paste0(names(globals$countries_available), collapse = "\", \""),
+            "\".")
     return(NULL)
   }
-  if (length(select) == 1 && select == "all") select <- names(globals$countries_available)
+  s <- if (length(select) == 1 && select == "all") {
+    names(globals$countries_available)
+  } else select
   pattern <- paste0(x, collapse = "|")
-  indices <- lapply(countries_available[fields], function(x) grep(pattern, x,
-                                                          ignore.case = TRUE))
+  indices <- lapply(globals$countries_available[f],
+                    function(x) grep(pattern, x, ignore.case = TRUE))
   while (length(indices) > 1) {
     indices <- c(list(union(indices[[1]], indices[[2]])), indices[-c(1, 2)])
   }
-  countries_available[unlist(indices), select] %>%
-    if (length(select) == 1) .[[1]] else .
+  ret <- globals$countries_available[unlist(indices), s]
+  if(nrow(ret) == 0) {
+    message("- Your search: match_countries(", deparse(x),
+            ", fields = ", deparse(fields),
+            ", select = ", deparse(select),
+            ", simplify = ", simplify, ") returned no countries.")
+    return(NULL)
+  }
+  ret %>% if (length(s) == 1 && simplify) .[[1]] else .
 }
