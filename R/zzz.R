4f796f0583839bc0b9d1c4547b0ac30e35d1240a
Pavel Vodrazka <vodrazka@svujihlava.cz>
2015-02-26 00:32:48 +0100

    custom_warning() added.

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,9 @@
 .onLoad <- function(libname, pkgname) {
   assign("globals", new.env(), envir=parent.env(environment()))
 }
+
+custom_warning <- function(subclass, message, call = sys.call(-1), ...) {
+  w <- simpleWarning(message, call = call, ...)
+  class(w) %<>% append(subclass)
+  warning(w)
+}
