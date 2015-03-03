5ebdf592e746e4540797391b43bb1c810e56405c
Pavel Vodrazka <vodrazka@svujihlava.cz>
2015-03-03 16:20:14 +0100

    Non-functional [.sinf removed.

--- R/zzz.R
+++ R/zzz.R
@@ -2,13 +2,9 @@
   assign("globals", new.env(), envir=parent.env(environment()))
 }
 
+#' @importFrom magrittr %<>%
 custom_warning <- function(subclass, message, call = sys.call(-1), ...) {
   w <- simpleWarning(message, call = call, ...)
   class(w) %<>% append(subclass)
   warning(w)
-}
-
-`[.sinf` <- function(x, i, j, drop = FALSE) {
-  res <- `[.tbl_df`(x, i, j, drop)
-  class(res) <- append(class(res), "sinf")
 }
