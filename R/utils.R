dae14e23a70e87d5c6397ab6766aa07ef46cce79
Pavel Vodrazka <vodrazka@svujihlava.cz>
2015-03-03 15:22:12 +0100

    Higher-level utility function update_sinf_data created.

---
+++ R/utils.R
@@ -0,0 +1,14 @@
+update_sinf_data <- function(file = "sinf_cache.rds", new_download = FALSE,
+                             suppress_messages = TRUE) {
+  plyr::a_ply(diseaseform_values$values_labels$disease_id_hidden, 1, get_sinf,
+        file = file, new_download = new_download,
+        suppress_messages = suppress_messages)
+  SINF <- read_cache(file)
+  if (is.null(SINF)) {
+    return(NULL)
+  } else {
+    save(SINF, file = "data/SINF.RData")
+    devtools::document(roclets=c('rd', 'collate', 'namespace'))
+    system("Rcmd.exe INSTALL --no-multiarch --with-keep.source .")
+  }
+}
